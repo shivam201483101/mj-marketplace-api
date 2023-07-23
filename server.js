765dd0279816b434c92ef26286473db13e2f332c
Shivam Pandey <shivam.201483101@vcet.edu.in>
2023-07-23 10:17:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,8 +44,15 @@ app.use((err, req, res, next) => {
 
   return res.status(errorStatus).send(errorMessage);
 });
+app.get('*',(req,res,next))=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
 
 app.listen(8800, () => {
   connect();
   console.log("Backend server is running!");
 });
+module.exports=app;
+
